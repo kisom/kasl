15f89614e29f3cd5f5e83e8c923b0a899f8875be
Kyle Isom <kyle@imap.cc>
2019-03-07 23:17:24 -0800

    Add example top-level source.

---
+++ examples/src/scheduling.cc
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <kasl/scheduling.h>
+#include <kasl/trigger.h>
+
+
+kasl::Trigger	triggerA(200);
+kasl::Trigger	triggerB(250);
+
+
+void
+thread_a()
+{
+	if (triggerA.ready()) {
+		Serial.print("+");
+	} else {
+		Serial.print("<");
+	}
+}
+
+
+
+void
+thread_b()
+{
+	if (triggerB.ready()) {
+		Serial.print("-");
+	} else {
+		Serial.print("!");
+	}
+}
+
+
+void
+setup()
+{
+	Serial.begin(9600);
+	while (!Serial) ;
+	delay(10000);
+
+	startThread(thread_a);
+	startThread(thread_b);
+	startScheduler();
+}
+
+
+void
+loop()
+{
+	runScheduler();
+	Serial.print(".");
+}
